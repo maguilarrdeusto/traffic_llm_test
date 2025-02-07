785458e81f746813f1bf2f3ed825cb30500c19f2
maguilarrdeusto <martin.aguilar@opendeusto.es>
2025-02-07 13:05:12 +0100

    Update settings.py

--- traffic_llm/settings.py
+++ traffic_llm/settings.py
@@ -17,18 +17,16 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zv4xrs#ks#=)9&=&djsas!ey+lyy)4-fnbr7#03qc80($x13p1'
 SECRET_KEY = os.environ.get('SECRET KEY', default='SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
